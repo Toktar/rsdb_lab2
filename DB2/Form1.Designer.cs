cebc69f848e8f7b44bcd7a70244dbc87a446d9d8
toktar <dizkida@gmail.com>
2016-10-25 12:52:56 +0300

    add client and transactions

---
+++ DB2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DB2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.transactionCheck = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(542, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(384, 96);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(542, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 143);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(914, 207);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(523, 125);
+            this.textBox1.TabIndex = 3;
+            // 
+            // transactionCheck
+            // 
+            this.transactionCheck.AutoSize = true;
+            this.transactionCheck.Location = new System.Drawing.Point(659, 119);
+            this.transactionCheck.Name = "transactionCheck";
+            this.transactionCheck.Size = new System.Drawing.Size(82, 17);
+            this.transactionCheck.TabIndex = 5;
+            this.transactionCheck.Text = "Transaction";
+            this.transactionCheck.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 362);
+            this.Controls.Add(this.transactionCheck);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox transactionCheck;
+    }
+}
+
---
+++ DB2/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DB2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection conn;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string serverName = "localhost"; // Адрес сервера (для локальной базы пишите "localhost")
+            string userName = "root"; // Имя пользователя
+            string dbName = "sakila"; //Имя базы данных
+            string port = "3306"; // Порт для подключения
+            string password = "root"; // Пароль для подключения
+            string connStr = "server=" + serverName +
+                ";user=" + userName +
+                ";database=" + dbName +
+                ";port=" + port +
+                ";password=" + password + ";";
+            conn = new MySqlConnection(connStr);
+
+            dataGridView1.DataError +=
+        new DataGridViewDataErrorEventHandler(dataGridView1_DataError);
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            string sql = "SELECT * FROM actor"; // Строка запроса
+            sql = textBox1.Text;
+            if (transactionCheck.Checked)
+            {
+                doTransactionQuery(sql);
+            }
+            else
+            {
+                doQuery(sql);
+            }
+
+
+
+            conn.Close();
+        }
+
+        private void doQuery(String query)
+        {
+
+            MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(query, conn);
+            DataSet DS = new DataSet();
+            try
+            {
+                mySqlDataAdapter.Fill(DS);
+
+               // dataGridView1 = startGridView;
+                
+
+                try
+                {
+                    dataGridView1.DataSource = DS.Tables[0];
+                }
+                catch (DataException dataExp)
+                {
+
+                }
+                this.dataGridView1.AutoResizeColumns(
+        DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+
+                
+            }
+            catch (MySqlException exp)
+            {
+                richTextBox1.Text = exp.Message;
+            }
+
+        }
+        private void dataGridView1_DataError(object sender,
+     DataGridViewDataErrorEventArgs e)
+        {
+            // If the data source raises an exception when a cell value is 
+            // commited, display an error message.
+            if (e.Exception != null &&
+                e.Context == DataGridViewDataErrorContexts.Commit)
+            {
+                MessageBox.Show("Error of Grid");
+            }
+        }
+        private void doTransactionQuery(String query)
+        {
+            MySqlTransaction tr = null;
+
+            String [] queryList = query.Split('\n');
+
+            try
+            {
+                
+                tr = conn.BeginTransaction();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.Transaction = tr;
+                foreach(String curQuery in queryList) {
+                    cmd.CommandText = curQuery;
+                    cmd.ExecuteNonQuery();
+                }
+              
+
+                tr.Commit();
+                richTextBox1.Text = "OK";
+
+            }
+            catch (MySqlException ex)
+            {
+                try
+                {
+                    tr.Rollback();
+
+                }
+                catch (MySqlException ex1)
+                {
+                     richTextBox1.Text =  ("Error: {0}" +  ex1.ToString());
+                }
+
+                richTextBox1.Text = ("Error: {0}" + ex.ToString());
+
+            }
+            finally
+            {
+
+                if (queryList.Length == 1)
+                {
+                    doQuery(queryList[0]);
+                }
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
